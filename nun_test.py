bcfcdec5712287181e693b0f29df3d60b4cc2aab
Josiah White <josiahwhite@gmail.com>
2018-10-18 23:11:12 -0400

    Attempting to invert x to be intuitive.
    Added nunchuk debug message

--- nun_test.py
+++ nun_test.py
@@ -2,7 +2,10 @@ from gpiozero import LED
 import Adafruit_PCA9685
 from nunchuck import nunchuck
 
-n = nunchuck()
+try:
+    n = nunchuck()
+except OSError:
+    raise OSError('Are you sure the Nunchuk is plugged in, idiot?')
 laser = LED(4)
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
@@ -14,13 +17,15 @@ nun_min = 45
 nun_max = 255
 nun_center = 127
 # Calculate the values for y = mx + b
-m = bound/(nun_max - nun_min)
-b = servo_center - m * nun_center
+xm = bound/(nun_min - nun_max)
+ym = bound/(nun_max - nun_min)
+xb = servo_center - xm * nun_center
+yb = servo_center - ym * nun_center
 while True:
     x, y = n.joystick()
     # set_pwm requires ints
-    x_servo = int(m*x + b)
-    y_servo = int(m*y + b)
+    x_servo = int(xm*x + xb)
+    y_servo = int(ym*y + yb)
     pwm.set_pwm(0, 0, x_servo)
     pwm.set_pwm(1, 0, y_servo)
     if n.button_c():
