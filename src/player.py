e87c9a9bf2112d21977c8302aeabbae9b102e0e3
JosiahDub <josiahwhite@gmail.com>
2018-10-19 20:23:32 -0400

    Creating player file to wrap servo and nunchuk

---
+++ src/player.py
@@ -0,0 +1,33 @@
+from nunchuck import nunchuck
+
+
+class Player:
+
+    # Range of the Nunchuk's analog stick
+    nun_min = 45
+    nun_max = 255
+    nun_center = 174
+
+    def __init__(self, bound, servo_center, pwm, x_channel, y_channel):
+        self.bound = bound
+        self.servo_center = servo_center
+        self.pwm = pwm
+        self.x_channel = x_channel
+        self.y_channel = y_channel
+        # Calculate the values for y = mx + b
+        # x needs to be swapped for our setup
+        self.xm = self.bound / (self.nun_min - self.nun_max)
+        self.ym = self.bound / (self.nun_max - self.nun_min)
+        self.xb = self.servo_center - self.xm * self.nun_center
+        self.yb = self.servo_center - self.ym * self.nun_center
+        try:
+            self.n = nunchuck()
+        except OSError:
+            raise OSError('Ensure the Nunchuk is plugged in') from None
+
+    def set_servo(self):
+        x, y = self.n.joystick()
+        x_servo = self.xm * x + self.xb
+        y_servo = self.ym * y + self.yb
+        self.pwm.set_pwm(self.x_channel, 0, x_servo)
+        self.pwm.set_pwm(self.y_channel, 0, y_servo)
