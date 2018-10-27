c840c2c7ae59ffa2f221b97c338a3f63b9ddce0c
JosiahDub <josiahwhite@gmail.com>
2018-10-27 17:29:27 -0400

    Catching pong up to speed. Created player 2's nunchuk

--- games/pong.py
+++ games/pong.py
@@ -1,9 +1,11 @@
 from src import Player, NPC
+from src.TURRETS import TURRET_1, TURRET_2, TURRET_3
+from src.NUNCHUKS import NUNCHUK_1, NUNCHUK_2
 
 
 class Pong:
 
-    def __init__(self, bound, servo_center, pwm):
-        self.player_1 = Player(bound, servo_center - bound/2, pwm, 0, 1, no_x=True)
-        self.player_2 = Player(bound, servo_center + bound/2, pwm, 4, 5, no_x=True)
-        self.ball = NPC(pwm, 2, 3)
+    def __init__(self, bound, pwm):
+        self.player_1 = Player(bound, bound, pwm, TURRET_1, NUNCHUK_1, no_x=True)
+        self.player_2 = Player(bound, bound, pwm, TURRET_3, NUNCHUK_2, no_x=True)
+        self.ball = NPC(pwm, TURRET_2)
--- src/NUNCHUKS.py
+++ src/NUNCHUKS.py
@@ -1,3 +1,4 @@
 from .player_nunchuk import PlayerNunchuk
 
 NUNCHUK_1 = PlayerNunchuk(127, 127, 20, 200, 20, 200)
+NUNCHUK_2 = PlayerNunchuk(127, 127, 20, 200, 20, 200)
