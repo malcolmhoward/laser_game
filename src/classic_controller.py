4899522c1fc44c1d5ab509d13dfc2c48e62b1b8a
JosiahDub <josiahwhite@gmail.com>
2018-10-28 13:58:11 -0400

    Adding more bare-bones functions. Added a map of data.

--- src/classic_controller.py
+++ src/classic_controller.py
@@ -1,3 +1,7 @@
+# links:
+# data http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Classic_Controller
+# C++ code https://github.com/dmadison/WiiChuck/tree/master/src
+
 from smbus2 import SMBus
 import RPi.GPIO as rpi
 import time as time
@@ -23,9 +27,77 @@ class ClassicController:
     def read(self):
         self.bus.write_byte(0x52, 0x00)
         time.sleep(self.delay)
-        temp = [(0x17 + (0x17 ^ self.bus.read_byte(0x52))) for i in range(6)]
+        temp = [(0x17 + (0x17 ^ self.bus.read_byte(0x52))) for _ in range(6)]
         return temp
 
-    def left_joy(self):
+    def left_joystick(self):
         data = self.read()
 
+    def right_joystick(self):
+        data = self.read()
+
+    def left_trigger_pressure(self):
+        data = self.read()
+
+    def right_trigger_pressure(self):
+        data = self.read()
+
+    def button_a(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_b(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_x(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_y(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_home(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_minus(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_plus(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_zl(self):
+        data = self.read()
+        return data[5] & 0x07
+
+    def button_zr(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_trigger_left(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_trigger_right(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_up(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_down(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_left(self):
+        data = self.read()
+        return data[5] & 0x00
+
+    def button_right(self):
+        data = self.read()
+        return data[5] & 0x00
