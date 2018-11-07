4e66daff14644287dc9c67003c5820380edfd321
Josiah White <josiahwhite@gmail.com>
2018-11-06 21:20:48 -0500

    Added a bigger loop to handle recreating the ball. Undone all the cheating

--- games/pong.py
+++ games/pong.py
@@ -24,44 +24,56 @@ class Pong:
         self.playing = False
 
     def play_on(self):
+        player_1_points = 0
+        player_2_points = 0
         self.playing = True
         self.ball.laser.on()
         self.player_1.laser.on()
         self.player_2.laser.on()
-        bounce = Bounce(self.center, self.center, 1.5707, 2)
-        ball_servo = self.ball.follow_path(bounce.data())
-        prev_time = 0
-        xs, ys = ball_servo.__next__()
         while self.playing:
-            curr_time = time.time()
-            if curr_time - prev_time >= self.time_rate:
-                prev_time = curr_time
-                self.player_1.set_servo()
-                self.player_2.set_servo()
-                xp_1, yp_1 = self.player_1.get_position()
-                xp_2, yp_2 = self.player_2.get_position()
-                xp_1_hit = fabs(xp_1 - xs) <= self.x_hit
-                xp_2_hit = fabs(xp_2 - xs) <= self.x_hit
-                yp_1_hit = fabs(yp_1 - ys) <= self.paddle_length
-                yp_2_hit = fabs(yp_2 - ys) <= self.paddle_length
-                horizontal_hit = False
-                vertical_hit = False
-                # Player 1 or 2 lose
-                if not yp_1_hit and xp_1_hit:
-                    print('Player 1 loses but continuing for now')
-                    vertical_hit = True
-                elif not yp_1_hit and xp_2_hit:
-                    print('Player 2 loses but continuing for now')
-                    vertical_hit = True
-                # Player 1 or 2 hit
-                if (yp_1_hit and xp_1_hit) or (yp_2_hit and xp_2_hit):
-                    vertical_hit = True
-                    horizontal_hit = False
-                # Top or bottom wall hit
-                elif ys >= (self.center + self.bound/2) or ys <= (self.center - self.bound/2):
-                    horizontal_hit = True
-                    vertical_hit = False
-                # else:
-                #     horizontal_hit = False
-                #     vertical_hit = False
-                xs, ys = ball_servo.send((horizontal_hit, vertical_hit))
+            bounce = Bounce(self.center, self.center, 1.5707, 2)
+            ball_servo = self.ball.follow_path(bounce.data())
+            prev_time = 0
+            xs, ys = ball_servo.__next__()
+            while self.playing:
+                curr_time = time.time()
+                if curr_time - prev_time >= self.time_rate:
+                    prev_time = curr_time
+                    self.player_1.set_servo()
+                    self.player_2.set_servo()
+                    xp_1, yp_1 = self.player_1.get_position()
+                    xp_2, yp_2 = self.player_2.get_position()
+                    xp_1_hit = fabs(xp_1 - xs) <= self.x_hit
+                    xp_2_hit = fabs(xp_2 - xs) <= self.x_hit
+                    yp_1_hit = fabs(yp_1 - ys) <= self.paddle_length
+                    yp_2_hit = fabs(yp_2 - ys) <= self.paddle_length
+                    # Player 1 or 2 lose
+                    if not yp_1_hit and xp_1_hit:
+                        print('Player 1 loses!')
+                        player_2_points += 1
+                        break
+                    elif not yp_1_hit and xp_2_hit:
+                        print('Player 2 loses!')
+                        player_1_points += 1
+                        break
+                    else:
+                        top_hit = ys >= (self.center + self.bound/2)
+                        bottom_hit = ys <= (self.center - self.bound/2)
+                        # Player 1 hit
+                        if yp_1_hit and xp_1_hit:
+                            print('Player 1 hit!')
+                            vertical_hit = True
+                            horizontal_hit = False
+                        # Player 2 hit
+                        elif yp_2_hit and xp_2_hit:
+                            print('Player 2 hit!')
+                            vertical_hit = True
+                            horizontal_hit = False
+                        # Top or bottom wall hit
+                        elif top_hit or bottom_hit:
+                            horizontal_hit = True
+                            vertical_hit = False
+                        else:
+                            horizontal_hit = False
+                            vertical_hit = False
+                        xs, ys = ball_servo.send((horizontal_hit, vertical_hit))
