d09df7b4901d1fd074e132d08a86e9d807d406ef
Josiah White <josiahwhite@gmail.com>
2018-11-06 21:00:34 -0500

    Changing pro controller to nunchuk

--- play_pong.py
+++ play_pong.py
@@ -1,10 +1,11 @@
 import Adafruit_PCA9685
 from games import Pong
 from src.TURRETS import TURRET_1, TURRET_2, TURRET_3
-from src import pro_controller_factory
+from src import pro_controller_factory, PlayerNunchuk
 
-controller_1, controller_2 = pro_controller_factory()
+nunchuk_1 = PlayerNunchuk(127, 127, 20, 200, 20, 200)
+# controller_1, controller_2 = pro_controller_factory()
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
-p = Pong(375, 100, pwm, controller_1, controller_2, TURRET_1, TURRET_3, TURRET_2)
+p = Pong(375, 100, pwm, nunchuk_1, nunchuk_1, TURRET_1, TURRET_3, TURRET_2)
 p.play_on()
