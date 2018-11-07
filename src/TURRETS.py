062ec94cb3378f74eb59a28ac4a5ad711c5856bb
Josiah White <josiahwhite@gmail.com>
2018-11-06 20:57:37 -0500

    Actually giving good laser values

--- src/TURRETS.py
+++ src/TURRETS.py
@@ -1,5 +1,5 @@
 from .turret import Turret
 
-TURRET_1 = Turret(0, 1, 0, 375, 375)
-TURRET_2 = Turret(2, 3, 0, 375, 375, x_cal=28, y_cal=18)
-TURRET_3 = Turret(4, 5, 0, 375, 375)
+TURRET_1 = Turret(0, 1, 5, 375, 375)
+TURRET_2 = Turret(2, 3, 4, 375, 375, x_cal=28, y_cal=18)
+TURRET_3 = Turret(4, 5, 6, 375, 375)
