e0e73211d548a0848dd5abeeca4ad11cbc78ef8a
Josiah White <josiahwhite@gmail.com>
2018-11-06 19:55:30 -0500

    Added an offset and fixed value option.

--- src/player.py
+++ src/player.py
@@ -4,16 +4,28 @@ from .player_controller import PlayerController
 
 class Player:
 
-    def __init__(self, x_bound: int, y_bound: int, pwm, turret: Turret,
+    def __init__(self,
+                 x_bound: int, y_bound: int,
+                 pwm, turret: Turret,
                  controller: PlayerController,
-                 no_x: bool=False, no_y: bool=False):
+                 x_offset: int=0, y_offset: int=0,
+                 no_x: bool=False, no_y: bool=False,
+                 fixed_x: int=0, fixed_y: int=0):
         self.controller = controller
         self.turret = turret
         self.pwm = pwm
         self.x_pin = turret.x_pin
         self.y_pin = turret.y_pin
+        self.x_offset = x_offset
+        self.y_offset = y_offset
         self.no_x = no_x
         self.no_y = no_y
+        self.fixed_x = fixed_x
+        if fixed_x:
+            pwm.set_pwm(self.x_pin, 0, fixed_x)
+        self.fixed_y = fixed_y
+        if fixed_y:
+            pwm.set_pwm(self.x_pin, 0, fixed_y)
         # Calculate the values for y = mx + b
         # x needs to be swapped for our setup
         self.xm = x_bound / (controller.x_min - controller.x_max)
@@ -28,13 +40,16 @@ class Player:
         self.servo_x = int(self.xm * x + self.xb)
         self.servo_y = int(self.ym * y + self.yb)
         if not self.no_x:
-            self.pwm.set_pwm(self.x_pin, 0, self.servo_x + self.turret.x_offset)
+            self.pwm.set_pwm(self.x_pin, 0, self.servo_x
+                                            + self.turret.x_cal
+                                            + self.x_offset)
         if not self.no_y:
-            self.pwm.set_pwm(self.y_pin, 0, self.servo_y + self.turret.y_offset)
+            self.pwm.set_pwm(self.y_pin, 0, self.servo_y
+                                            + self.turret.y_cal
+                                            + self.y_offset)
 
     def get_position(self):
-        # TODO: With offset?
-        return self.servo_x, self.servo_y
+        return self.servo_x + self.x_offset, self.servo_y + self.y_offset
 
     def firing(self):
         return self.controller.fire()
