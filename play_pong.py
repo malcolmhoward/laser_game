b7b4e8ed4ae37418301770e2123006c86b4f1191
Josiah White <josiahwhite@gmail.com>
2018-11-07 21:48:12 -0500

    Fixed bug with player losing logic. Added and moved around print statements

--- play_pong.py
+++ play_pong.py
@@ -1,11 +1,11 @@
 import Adafruit_PCA9685
 from games import Pong
 from src.TURRETS import TURRET_1, TURRET_2, TURRET_3
-from src import pro_controller_factory, PlayerNunchuk
+from src import pro_controller_factory
 
-nunchuk_1 = PlayerNunchuk(127, 127, 20, 200, 20, 200)
-# controller_1, controller_2 = pro_controller_factory()
+# nunchuk_1 = PlayerNunchuk(127, 127, 20, 200, 20, 200)
+c_1, c_2 = pro_controller_factory()
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
-p = Pong(375, 100, pwm, nunchuk_1, nunchuk_1, TURRET_1, TURRET_3, TURRET_2)
+p = Pong(375, 100, pwm, c_1, c_2, TURRET_1, TURRET_3, TURRET_2)
 p.play_on()
