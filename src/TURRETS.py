85ce376bfb88e640ea8f57551096fea1d746bd9f
Josiah White <josiahwhite@gmail.com>
2018-11-10 15:39:50 -0500

    Changing pins to match reality

--- src/TURRETS.py
+++ src/TURRETS.py
@@ -1,5 +1,5 @@
 from .turret import Turret
 
-TURRET_1 = Turret(0, 1, 5, 375, 375, x_cal=-40, y_cal=0)
-TURRET_2 = Turret(2, 3, 4, 375, 375, x_cal=28, y_cal=18)
-TURRET_3 = Turret(4, 5, 6, 375, 375, x_cal=50, y_cal=-6)
+TURRET_1 = Turret(0, 1, 17, 375, 375, x_cal=-40, y_cal=0)
+TURRET_2 = Turret(2, 3, 18, 375, 375, x_cal=28, y_cal=18)
+TURRET_3 = Turret(4, 5, 4, 375, 375, x_cal=50, y_cal=-6)
