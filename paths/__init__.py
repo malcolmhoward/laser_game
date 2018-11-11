d5079d2dc6c80f71e3ce8e8d07b6bfeadafc60c0
Josiah White <josiahwhite@gmail.com>
2018-11-11 17:02:07 -0500

    Fleshed out data

--- paths/__init__.py
+++ paths/__init__.py
@@ -1,3 +1,4 @@
 from .line import Line
 from .circle import Circle
 from .bounce import Bounce
+from .corkscrew import Corkscrew
---
+++ paths/corkscrew.py
@@ -0,0 +1,30 @@
+from math import sin, cos, atan2, pi, fabs
+from .circle import Circle
+
+
+class Corkscrew(Circle):
+    
+    def __init__(self, x_center, y_center, x_end, y_end, radius, angle, angular_rate, linear_rate, clockwise=True):
+        super().__init__(x_center, y_center, radius, angle, angular_rate, clockwise)
+        self.x_end = x_end
+        self.y_end = y_end
+        self._linear_rate = linear_rate
+        self.linear_angle = atan2(y_center - y_end, x_center - x_end)
+        self.x_rate = linear_rate * cos(self.linear_angle)
+        self.y_rate = linear_rate * sin(self.linear_angle)
+        
+    def data(self):
+        x = self._radius * sin(self._angle) + self.x_center
+        y = self._radius * cos(self._angle) + self.y_center
+        yield int(x), int(y)
+        angle_rate = self._angle + self._rate
+        while fabs(x - self.x_end) > fabs(self.x_rate) and fabs(y - self.y_end) > fabs(self.y_rate):
+            x = self._radius * sin(angle_rate) + self.x_center + self.x_rate
+            y = self._radius * cos(angle_rate) + self.y_center + self.y_rate
+            yield int(x), int(y)
+            angle_rate += self._rate
+            # Check to normalize
+            if angle_rate >= 2 * pi:
+                angle_rate -= 2 * pi
+            elif angle_rate <= -2 * pi:
+                angle_rate += 2 * pi
