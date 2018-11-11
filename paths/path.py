f0525d57ff88cfcb94f947b72ead2faa4f2f6ff4
Josiah White <josiahwhite@gmail.com>
2018-11-11 16:09:04 -0500

    Changing rate name to allow @property usage

--- paths/path.py
+++ paths/path.py
@@ -3,7 +3,7 @@ from typing import Generator, Tuple
 
 class Path:
     def __init__(self, rate):
-        self.rate = rate
+        self._rate = rate
 
     def data(self) -> Generator[Tuple[int, int], None, None]:
         ...
