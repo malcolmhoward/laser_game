db9976901252f474c76a559117490a4c330c4b75
Josiah White <josiahwhite@gmail.com>
2018-11-10 23:39:42 -0500

    Added docstrings.\nMoved player firing logic to own function\nAdded Fixme for line premature StopIteration

--- games/missile_defense.py
+++ games/missile_defense.py
@@ -7,17 +7,37 @@ from src import Player, NPC
 from .game import Game
 from src.player_controller import PlayerController
 from src.turret import Turret
+# TODO: Add lives laser that slowly trickles down
 
 
 class MissileDefense(Game):
+    """
+    The player must aim and shoot at missile raining down on a peaceful city.
+    """
 
+    '''
+    GAME INFO
+    '''
+    curr_time = 0
+
+    '''
+    PLAYER INFO
+    '''
+    # Delay until the player can fire again
+    player_attack_rate = 1
+    # Size of player attack
     bomb_radius = 10
+    player_fire = False
+    fire_time = 0
+    skip_frame = False
+    laser_blink = False
+    '''
+    MISSILE INFO
+    '''
     # How long to delay until the missile comes alive
     missile_delay = 1
     # How much to increase the missile speed per success
     rate_increase = 0.1
-    # Delay until the player can fire again
-    player_attack_rate = 0.5
 
     def __init__(self, center, bound, pwm,
                  controller: PlayerController,
@@ -28,14 +48,12 @@ class MissileDefense(Game):
         self.player.laser.on()
         self.missile = NPC(pwm, missile_turret)
         self.missile.laser.on()
+        self.player_fired = False
 
     def play_on(self):
         self.playing = True
         hit = False
         lose = False
-        player_fired = False
-        laser_blink = False
-        player_fire_time = -1
         prev_time = 0
         player_score = 0
         homes_destroyed = 0
@@ -56,9 +74,9 @@ class MissileDefense(Game):
                 missile = self.make_missile(missile_rate)
                 missile_respawn = True
                 respawn_time = time.time()
-            curr_time = time.time()
-            if curr_time - prev_time >= self.time_rate:
-                prev_time = curr_time
+            self.curr_time = time.time()
+            if self.curr_time - prev_time >= self.time_rate:
+                prev_time = self.curr_time
                 p_x, p_y = self.player.set_servo()
                 if not missile_respawn:
                     try:
@@ -66,28 +84,44 @@ class MissileDefense(Game):
                     except StopIteration:
                         lose = True
                     else:
-                        if not player_fired:
+                        if not self.player_fired:
                             if self.player.firing():
-                                player_fired = True
-                                player_fire_time = time.time()
+                                self.player_fired = True
+                                self.fire_time = time.time()
                                 dist_2_bomb = sqrt((m_y - p_y)**2 + (m_x - p_x)**2)
                                 if dist_2_bomb <= self.bomb_radius:
                                     hit = True
                 else:
-                    if curr_time - respawn_time > self.missile_delay:
+                    if self.curr_time - respawn_time > self.missile_delay:
                         missile_respawn = False
                         self.missile.laser.on()
-                # Handle player blinking even when missile respawning
-                if player_fired:
-                    if laser_blink:
-                        self.player.laser.on()
-                    else:
-                        self.player.laser.off()
-                    laser_blink = not laser_blink
-                    if curr_time - player_fire_time > self.player_attack_rate:
-                        player_fired = False
+                self.handle_player_firing()
+
+    def handle_player_firing(self):
+        """
+        Handles all player info after firing, such as laser blinking
+        :return:
+        """
+        if self.player_fired:
+            if not self.skip_frame:
+                self.skip_frame = True
+                if self.laser_blink:
+                    self.player.laser.on()
+                else:
+                    self.player.laser.off()
+                    self.laser_blink = not self.laser_blink
+            else:
+                self.skip_frame = False
+            if self.curr_time - self.fire_time > self.player_attack_rate:
+                self.player_fired = False
+                self.player.laser.on()
 
     def make_missile(self, rate=1) -> Generator:
+        """
+        Creates a new random missile
+        :param rate:
+        :return:
+        """
         y_low = int(self.center + self.bound / 2)
         y_high = int(self.center - self.bound/2)
         x_start = random.randint(y_high, y_low)
@@ -96,5 +130,6 @@ class MissileDefense(Game):
         missile = self.missile.follow_path(path.data())
         # Let the servos get into position. Yes, yield twice
         missile.__next__()
+        # FIXME: If x or y are too close to each other, this can lead to premature StopIteration
         missile.__next__()
         return missile
