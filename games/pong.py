0fa5a2a584c7aad8559153dd0ef9967aa24aa2ad
Josiah White <josiahwhite@gmail.com>
2018-11-14 21:49:09 -0500

    Adopting missile defense ideas of a blinking LED during reset

--- games/pong.py
+++ games/pong.py
@@ -6,9 +6,25 @@ from paths import Bounce
 
 
 class Pong(Game):
+    """
+    Two players swat a ball back and forth.
+    """
 
+    '''
+    PLAYER INFO
+    '''
     paddle_length = 10
     x_hit = 2
+
+    '''
+    BALL INFO
+    '''
+    ball_rate = 2
+    resetting = False
+    skip_frame = False
+    laser_blink = False
+    reset_time = 0
+    reset_rate = 1
 
     def __init__(self, center, bound, pwm, player_1_controller, player_2_controller,
                  player_1_turret, player_2_turret, npc_turret):
@@ -29,10 +45,9 @@ class Pong(Game):
         self.player_1.laser.on()
         self.player_2.laser.on()
         while self.playing:
-            bounce = Bounce(self.center, self.center, 1.5707, 2)
-            ball_servo = self.ball.follow_path(bounce.data())
+            ball = self.make_ball(self.ball_rate)
             prev_time = 0
-            xs, ys = ball_servo.__next__()
+            xs, ys = ball.send((False, False))
             while self.playing:
                 curr_time = time.time()
                 if curr_time - prev_time >= self.time_rate:
@@ -50,10 +65,12 @@ class Pong(Game):
                     if not yp_1_hit and xp_1_hit:
                         print('Player 1 loses!')
                         player_2_points += 1
+                        self.resetting = True
                         break
                     elif not yp_2_hit and xp_2_hit:
                         print('Player 2 loses!')
                         player_1_points += 1
+                        self.resetting = True
                         break
                     else:
                         top_hit = ys >= (self.center + self.bound/2)
@@ -75,4 +92,34 @@ class Pong(Game):
                         else:
                             horizontal_hit = False
                             vertical_hit = False
-                        xs, ys = ball_servo.send((horizontal_hit, vertical_hit))
+                        if not self.resetting:
+                            xs, ys = ball.send((horizontal_hit,
+                                                vertical_hit))
+                    self.handle_ball_resetting()
+
+    def handle_ball_resetting(self):
+        """
+        Handles all player info after firing, such as laser blinking
+        :return:
+        """
+        if self.resetting:
+            if not self.skip_frame:
+                self.skip_frame = True
+                if self.laser_blink:
+                    self.ball.laser.on()
+                else:
+                    self.ball.laser.off()
+                self.laser_blink = not self.laser_blink
+            else:
+                self.skip_frame = False
+            if self.curr_time - self.reset_time > self.reset_rate:
+                self.resetting = False
+                self.ball.laser.on()
+
+    def make_ball(self, rate=2):
+        bounce = Bounce(self.center, self.center, 1.5707, rate)
+        ball_servo = self.ball.follow_path(bounce.data())
+        # Let the servos get into position. Yes, yield twice
+        ball_servo.__next__()
+        ball_servo.send((False, False))
+        return ball_servo
