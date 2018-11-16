9fa46307cfdf9675b461aec737311ad00799b2ed
Josiah White <josiahwhite@gmail.com>
2018-11-15 20:41:27 -0500

    CReating file

---
+++ play_maze.py
@@ -0,0 +1,12 @@
+import Adafruit_PCA9685
+from games import Maze
+from src.TURRETS import TURRET_1
+from src import pro_controller_factory
+
+left, right = pro_controller_factory()
+
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(60)
+
+maze = Maze(375, 100, pwm, left, TURRET_1)
+maze.play_on()
