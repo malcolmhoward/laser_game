9f8b6740470e0323d97091624e0eadc1f9d90c96
JosiahDub <josiahwhite@gmail.com>
2018-11-17 21:20:11 -0500

    All classes filled out. Added some protections for bad data

--- paths/math/Spirograph.py
+++ paths/math/Spirograph.py
@@ -1,4 +1,4 @@
-from math import cos, sin, pi, fabs
+from math import cos, sin, pi
 from ..path import Path
 
 
@@ -6,13 +6,17 @@ class Spirograph(Path):
 
     def __init__(self, rate, big_r, little_r):
         super().__init__(rate)
+        if little_r == 0:
+            raise ValueError('Little r cannot be zero.')
         self.big_r = big_r
         self.little_r = little_r
 
 
 class Hypo(Spirograph):
     def __init__(self, rate, big_r, little_r):
-        super(Spirograph).__init__(rate, big_r, little_r)
+        if little_r == big_r:
+            raise ValueError('Little r and Big R cannot be equal.')
+        super().__init__(rate, big_r, little_r)
         self.ratio = self.big_r - self.little_r
 
 
@@ -25,7 +29,7 @@ class Epi(Spirograph):
 class Hypotrochoid(Hypo):
 
     def __init__(self, rate, big_r, little_r, d):
-        super(Hypo).__init__(rate, big_r, little_r)
+        super().__init__(rate, big_r, little_r)
         self.d = d
 
     def data(self):
@@ -33,7 +37,7 @@ class Hypotrochoid(Hypo):
         while True:
             x = self.ratio * cos(angle) + self.d * cos(self.ratio / self.little_r * angle)
             y = self.ratio * sin(angle) - self.d * sin(self.ratio / self.little_r * angle)
-            yield x, y
+            yield int(x), int(y)
             # iterate angle and normalize
             angle += self.rate
             if angle >= 2 * pi:
@@ -45,10 +49,20 @@ class Hypotrochoid(Hypo):
 class Hypocycloid(Hypo):
 
     def __init__(self, rate, big_r, little_r):
-        super(Hypo).__init__(rate, big_r, little_r)
+        super().__init__(rate, big_r, little_r)
 
     def data(self):
-        pass
+        angle = 0
+        while True:
+            x = self.ratio * cos(angle) + self.little_r * cos(self.ratio / self.little_r * angle)
+            y = self.ratio * sin(angle) - self.little_r * sin(self.ratio / self.little_r * angle)
+            yield int(x), int(y)
+            # iterate angle and normalize
+            angle += self.rate
+            if angle >= 2 * pi:
+                angle -= 2 * pi
+            elif angle <= -2 * pi:
+                angle += 2 * pi
 
 
 class Epicycloid(Epi):
@@ -56,8 +70,35 @@ class Epicycloid(Epi):
     def __init__(self, rate, big_r, little_r):
         super().__init__(rate, big_r, little_r)
 
+    def data(self):
+        angle = 0
+        while True:
+            x = self.ratio * cos(angle) - self.little_r * cos(self.ratio / self.little_r * angle)
+            y = self.ratio * sin(angle) - self.little_r * sin(self.ratio / self.little_r * angle)
+            yield int(x), int(y)
+            # iterate angle and normalize
+            angle += self.rate
+            if angle >= 2 * pi:
+                angle -= 2 * pi
+            elif angle <= -2 * pi:
+                angle += 2 * pi
+
 
 class Epitrochoid(Epi):
 
-    def __init__(self, rate, big_r, little_r):
-        super().__init__(rate, big_r, little_r)
+    def __init__(self, rate, big_r, little_r, d):
+        super().__init__(rate, big_r, little_r,)
+        self.d = d
+
+    def data(self):
+        angle = 0
+        while True:
+            x = self.ratio * cos(angle) - self.d * cos(self.ratio / self.little_r * angle)
+            y = self.ratio * sin(angle) - self.d * sin(self.ratio / self.little_r * angle)
+            yield int(x), int(y)
+            # iterate angle and normalize
+            angle += self.rate
+            if angle >= 2 * pi:
+                angle -= 2 * pi
+            elif angle <= -2 * pi:
+                angle += 2 * pi
