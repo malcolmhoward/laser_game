84ebbc441c2cdcf82ad200eae6d579e70424404b
Josiah White <josiahwhite@gmail.com>
2018-12-02 11:28:36 -0500

    Player laser set right. Adding initial values. Returning x y

--- nun_test.py
+++ nun_test.py
@@ -6,10 +6,9 @@ from src.TURRETS import TURRET_1, TURRET_2, TURRET_3
 
 # c = PlayerNunchuk(127, 127, 20, 200, 20, 200)
 c, _ = pro_controller_factory()
-laser = LED(4)
-laser.on()
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
-player_1 = Player(100, 100, pwm, TURRET_1, c)
+player_1 = Player(100, 100, pwm, TURRET_1, c, initial_x=375, initial_y=375)
+player_1.laser.on()
 while True:
-    player_1.manual_servo()
+    x, y = player_1.manual_servo()
