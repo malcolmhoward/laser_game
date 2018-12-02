92031eefd67728557a91d0c741d721ba6d36b71d
Josiah White <josiahwhite@gmail.com>
2018-12-02 11:22:16 -0500

    Testing out manual controls

--- nun_test.py
+++ nun_test.py
@@ -1,14 +1,15 @@
 from gpiozero import LED
 import Adafruit_PCA9685
 from src.player import Player
-from src import PlayerNunchuk
+from src import PlayerNunchuk, pro_controller_factory
 from src.TURRETS import TURRET_1, TURRET_2, TURRET_3
 
-nunchuk = PlayerNunchuk(127, 127, 20, 200, 20, 200)
+# c = PlayerNunchuk(127, 127, 20, 200, 20, 200)
+c, _ = pro_controller_factory()
 laser = LED(4)
 laser.on()
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
-player_1 = Player(100, 100, pwm, TURRET_1, nunchuk)
+player_1 = Player(100, 100, pwm, TURRET_1, c)
 while True:
-    player_1.set_servo()
+    player_1.manual_servo()
