14dfe27d6422ccd7f1240e9c9f8081a67cb2470d
Josiah White <josiahwhite@gmail.com>
2018-12-02 13:13:18 -0500

    Adding center values

--- nun_test.py
+++ nun_test.py
@@ -8,7 +8,9 @@ from src.TURRETS import TURRET_1, TURRET_2, TURRET_3
 c, _ = pro_controller_factory()
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
-player_1 = Player(100, 100, pwm, TURRET_1, c, initial_x=375, initial_y=375)
+player_1 = Player(100, 100, pwm, TURRET_1, c,
+                  initial_x=375, initial_y=375,
+                  x_center=375, y_center=375)
 player_1.laser.on()
 while True:
     x, y = player_1.manual_servo()
