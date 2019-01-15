6f9dbfebd8134a348abfb2199bcefb84de5be13e
Josiah White <josiahwhite@gmail.com>
2019-01-15 18:38:53 -0500

    Adding support for turrets 4 and 5.

--- src/TURRETS.py
+++ src/TURRETS.py
@@ -4,9 +4,13 @@ from .turret import Turret
 with open('src/calibration.json', 'r') as json_file:
     cal = json.load(json_file)
 
-TURRET_1 = Turret(0, 1, 17, 375, 375, x_cal=cal['turret1']['x'],
+TURRET_1 = Turret(0, 1, 4, 375, 375, x_cal=cal['turret1']['x'],
                                       y_cal=cal['turret1']['y'])
-TURRET_2 = Turret(2, 3, 18, 375, 375, x_cal=cal['turret2']['x'],
+TURRET_2 = Turret(2, 3, 17, 375, 375, x_cal=cal['turret2']['x'],
                                       y_cal=cal['turret2']['y'])
-TURRET_3 = Turret(4, 5, 4, 375, 375, x_cal=cal['turret3']['x'],
+TURRET_3 = Turret(4, 5, 18, 375, 375, x_cal=cal['turret3']['x'],
                                      y_cal=cal['turret3']['y'])
+TURRET_4 = Turret(6, 7, 27, 375, 375, x_cal=cal['turret4']['x'],
+                                     y_cal=cal['turret4']['y'])
+TURRET_5 = Turret(8, 9, 22, 375, 375, x_cal=cal['turret5']['x'],
+                                     y_cal=cal['turret6']['y'])
