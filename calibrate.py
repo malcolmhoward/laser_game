97c9c72d85dfc0a0a4f355c8f7f9a8531c18137a
Josiah White <josiahwhite@gmail.com>
2019-01-15 18:50:59 -0500

    Adding support for turrets 4 and 5.

--- calibrate.py
+++ calibrate.py
@@ -2,17 +2,22 @@ import json
 import time
 import Adafruit_PCA9685
 from src.player import Player
-from src import PlayerNunchuk, pro_controller_factory
-from src.TURRETS import TURRET_1, TURRET_2, TURRET_3
+from src import PlayerNunchuk
+from src.TURRETS import TURRET_1, TURRET_2, TURRET_3, TURRET_4, TURRET_5
 
 with open('src/calibration.json', 'r') as json_file:
     curr_cal = json.load(json_file)
 
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
-# c = PlayerNunchuk(127, 127, 20, 200, 20, 200)
-c, _ = pro_controller_factory()
-turrets = {TURRET_1: '1', TURRET_2: '2', TURRET_3: '3'}
+c = PlayerNunchuk(127, 127, 20, 200, 20, 200)
+# c, _ = pro_controller_factory()
+turrets = {TURRET_1: '1',
+           TURRET_2: '2',
+           TURRET_3: '3',
+           TURRET_4: '4',
+           TURRET_5: '5',
+}
 for turret, num in turrets.items():
     player = Player(100, 100, pwm, turret, c)
     player.laser.on()
